977e468039d1b0ecf9ba2a29a4e3651b9a64fd56
Amber Taranto <ambzzzz@gmail.com>
2019-06-05 21:09:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,9 @@ def oxford_comma(array)
     if array.size == 2
         return array.join(" and ")
     end
+    last_item = array.pop
+    array.join("and last_item")
     return array.join(", ")
+
 
 end
